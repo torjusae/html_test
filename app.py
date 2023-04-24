ce7f12e297331db760167fecba0dcb5c9e8136ea
Torjus Nilsen <torjus.nilsen@glitreenergi.no>
2023-04-24 08:47:35 +0200

    added html

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ import requests
 import streamlit as st
 import streamlit.components.v1 as components
 
-
 with st.container():
     HtmlFile = open("geithusfoss_export_html.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
     print(source_code)
     components.html(source_code)
-    
