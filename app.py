7eff9bf658eb0e0b5b588922d193a86fb0dfa9da
torjusae <131646543+torjusae@users.noreply.github.com>
2023-04-24 08:45:44 +0200

    big bang

---
+++ app.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import requests
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+with st.container():
+    HtmlFile = open("geithusfoss_export_html.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    print(source_code)
+    components.html(source_code)
+    
